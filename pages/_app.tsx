0ad0b468deba501f873dee8cbe1ad6e422c29d14
cutlope <abdullah77@outlook.com>
2022-10-27 00:43:35 +0300

    fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Header, Footer, Tilde } from '@components'
 import NextNProgress from 'nextjs-progressbar'
+// eslint-disable-next-line import/no-unresolved
 import { Analytics } from '@vercel/analytics/react'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
@@ -31,6 +32,23 @@ export default function MyApp({ Component, pageProps }: AppProps) {
       <div className='container mx-auto px-6 md:max-w-4xl md:px-4'>
         <Header />
         <NextNProgress color='#A0A0A0' options={{ showSpinner: false }} />
+        <div className='fixed top-0 right-0 z-50 m-8 flex h-6 w-6 items-center justify-center rounded-full bg-gray-700 p-3 font-mono text-xs text-white sm:bg-pink-500 md:bg-orange-500 lg:bg-green-500 xl:bg-blue-500'>
+          <div className='block sm:hidden md:hidden lg:hidden xl:hidden'>
+            al
+          </div>
+          <div className='hidden sm:block md:hidden lg:hidden xl:hidden'>
+            sm
+          </div>
+          <div className='hidden sm:hidden md:block lg:hidden xl:hidden'>
+            md
+          </div>
+          <div className='hidden sm:hidden md:hidden lg:block xl:hidden'>
+            lg
+          </div>
+          <div className='hidden sm:hidden md:hidden lg:hidden xl:block'>
+            xl
+          </div>
+        </div>
         <Component {...pageProps} />
         <Analytics />
         <Footer />
