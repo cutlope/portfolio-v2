3e2afd38fafd108f59a917e841ec3510b417cbe9
cutlope <abdullah77@outlook.com>
2022-11-21 00:55:26 +0300

    darkmode fix

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,9 @@ import Script from 'next/script'
 const modeScript = `
   let darkModeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)')
 
-  window.localStorage.isDarkMode = 'true'
+  if (!window.localStorage.getItem('isDarkMode')) {
+        window.localStorage.isDarkMode = 'true'
+    }
   updateMode()
   darkModeMediaQuery.addEventListener('change', updateModeWithoutTransitions)
   window.addEventListener('storage', updateModeWithoutTransitions)
@@ -14,20 +16,20 @@ const modeScript = `
     let isDarkMode = window.localStorage.isDarkMode === 'true' || (!('isDarkMode' in window.localStorage) && isSystemDarkMode)
 
     if (isDarkMode) {
-      document.documentElement.classList.add('dark')
+        document.documentElement.classList.add('dark')
     } else {
-      document.documentElement.classList.remove('dark')
+        document.documentElement.classList.remove('dark')
     }
 
     if (isDarkMode === isSystemDarkMode) {
-      delete window.localStorage.isDarkMode
+        delete window.localStorage.isDarkMode
     }
   }
 
   function disableTransitionsTemporarily() {
     document.documentElement.classList.add('[&_*]:!transition-none')
     window.setTimeout(() => {
-      document.documentElement.classList.remove('[&_*]:!transition-none')
+        document.documentElement.classList.remove('[&_*]:!transition-none')
     }, 0)
   }
 
